22a7caede5e7d0681e6dd0242a94574f21eea46a
ardyansatya <ardyan.satya@gmail.com>
2020-02-11 15:00:17 +0700

    add new file

---
+++ cerita.in/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Cerita.in</title>
+  </head>
+  <body>
+    <input type="text" name="" value="Judul">
+
+  </body>
+</html>
